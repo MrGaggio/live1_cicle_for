f72c9bb9e78f94cb936d2352d16d109385a07bc3
MrGaggio <steruggiu@gmail.com>
2021-10-13 11:18:50 +0100

    push

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+// stampare tutti i numeri da 0 sino a 1000
+/* 
+for (let i=0; i <=1000; i++) {
+    console.log(i);
+    document.getElementById("number").innerHTML +=i;
+}
+
+
+ */
+
+// stampare tutti i numeri da 1000 sino a 0
+
+for (let i=1000; i>=0; i-- ) {
+    console.log(i);
+    document.getElementById("number").innerHTML += i;
+}
